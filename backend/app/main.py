f2070c85081af3e4c64e1367bc83ae69eb79b7b3
Muhammad Usman <179584824+usman-pirzada@users.noreply.github.com>
2026-09-12 15:05:53 +0500

    enable frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.database.init_db import initialize_database
@@ -11,6 +12,14 @@ from app.routers import (
 )
 
 app = FastAPI(title=settings.app_name)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
