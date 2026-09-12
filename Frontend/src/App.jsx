fcab0fc716c32eb2bd70c8681817c62b6ee07606
MujtabaKhanGhauri <m.mujtaba.khan.ghauri@gmail.com>
2026-09-12 17:56:21 +0500

    Frontend setup kr dia & sample working correctly.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold">
+          ExpenseMate
+        </h1>
+      </div>
+    )
+  }
+  
+export default App
+  
